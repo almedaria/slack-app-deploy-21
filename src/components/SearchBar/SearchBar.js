2d95985cfda4f344386a53fe736999571ceb32b7
almedaria <rmmgalmeda@gmail.com>
2021-10-24 03:48:14 +0800

    added UseEffect in sidebar.js and searchbar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import SearchResult from "./SearchResult";
 import Headers from "../../Helpers/Headers";
 import { useEffect } from "react/cjs/react.development";
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddChannel from "../Channel/AddChannel";
 import Headers from "../../Helpers/Headers";
 import "./Sidebar.css";
